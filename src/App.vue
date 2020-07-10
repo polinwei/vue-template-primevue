9b01c2528d23e88a597088eb8ae621b78dca99d5
polin.wei <polin.wei@gmail.com>
2020-07-10 16:48:44 +0800

    將多個 Vue Router 檔案合併成一個設定檔

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <template>
   <div :class="containerClass" @click="onWrapperClick">
     <LayoutTopbar @menu-toggle="onMenuToggle" />
+
+    <transition name="layout-sidebar">
+      <div :class="sidebarClass" @click="onSidebarClick" v-show="isSidebarVisible()">
+        <div class="layout-logo">
+            <router-link to="/">
+                <img alt="Logo" :src="logo" />
+            </router-link>
+        </div>
+        <LayoutProfile />
+        <LayoutMenu :model="menu" @menuitem-click="onMenuItemClick" />
+      </div>
+
+    </transition>
+
+    <div class="layout-main">
+			<router-view />
+		</div>
+
   </div>
 </template>
 
 <script>
-import LayoutTopbar from "./components/layout/LayoutTopbar.vue";
+import LayoutTopbar from './components/layout/LayoutTopbar.vue';
+import LayoutProfile from './components/layout/LayoutProfile.vue';
+import LayoutMenu from './components/layout/LayoutMenu.vue'
 export default {
   data() {
     return {
@@ -13,7 +33,25 @@ export default {
       layoutColorMode: "dark",
       staticMenuInactive: false,
       overlayMenuActive: false,
-      mobileMenuActive: false
+      mobileMenuActive: false,
+      menu : [
+              {label: 'Welcome', icon: 'pi pi-fw pi-home', to: '/'},
+              {label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/Dashboard'},
+              {
+                  label: 'Menu Modes', icon: 'pi pi-fw pi-cog',
+                  items: [
+                      {label: 'Static Menu', icon: 'pi pi-fw pi-bars',  command: () => this.layoutMode = 'static' },
+                      {label: 'Overlay Menu', icon: 'pi pi-fw pi-bars',  command: () => this.layoutMode = 'overlay' }
+                  ]
+              },
+              {
+                  label: 'Menu Colors', icon: 'pi pi-fw pi-align-left', 
+                  items: [
+                      {label: 'Dark', icon: 'pi pi-fw pi-bars',  command: () => this.layoutColorMode = 'dark' },
+                      {label: 'Light', icon: 'pi pi-fw pi-bars',  command: () => this.layoutColorMode = 'light' }
+                  ]
+              },
+      ]
     };
   },
   methods: {
@@ -27,7 +65,7 @@ export default {
     },
     onMenuToggle() {
       this.menuClick = true;
-
+      
       if (this.isDesktop()) {
         if (this.layoutMode === "overlay") {
           this.overlayMenuActive = !this.overlayMenuActive;
@@ -42,7 +80,29 @@ export default {
     },
     isDesktop() {
       return window.innerWidth > 1024;
-    }
+    },
+    isSidebarVisible() {
+      if (this.isDesktop()) {
+          if (this.layoutMode === 'static')
+              return !this.staticMenuInactive;
+          else if (this.layoutMode === 'overlay')
+              return this.overlayMenuActive;
+          else
+              return true;
+      }
+      else {
+          return true;
+      }
+    },
+    onSidebarClick() {
+        this.menuClick = true;
+    },
+    onMenuItemClick(event) {
+        if (event.item && !event.item.items) {
+            this.overlayMenuActive = false;
+            this.mobileMenuActive = false;
+        }
+    }, 
   },
   computed: {
     containerClass() {
@@ -51,17 +111,26 @@ export default {
         {
           "layout-overlay": this.layoutMode === "overlay",
           "layout-static": this.layoutMode === "static",
-          "layout-static-sidebar-inactive":
-            this.staticMenuInactive && this.layoutMode === "static",
-          "layout-overlay-sidebar-active":
-            this.overlayMenuActive && this.layoutMode === "overlay",
+          "layout-static-sidebar-inactive": this.staticMenuInactive && this.layoutMode === "static",
+          "layout-overlay-sidebar-active": this.overlayMenuActive && this.layoutMode === "overlay",
           "layout-mobile-sidebar-active": this.mobileMenuActive
         }
       ];
+    },
+    sidebarClass() {
+      return ['layout-sidebar', {
+          'layout-sidebar-dark': this.layoutColorMode === 'dark',
+          'layout-sidebar-light': this.layoutColorMode === 'light'
+      }];
+    },
+    logo() {
+        return (this.layoutColorMode === 'dark') ? "assets/layout/images/logo-white.svg" : "assets/layout/images/logo.svg";
     }
   },
   components: {
-    LayoutTopbar: LayoutTopbar
+    'LayoutTopbar': LayoutTopbar,
+    'LayoutProfile': LayoutProfile,
+    'LayoutMenu': LayoutMenu,
   }
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import RouterDashboard from './RouterDashboard'
+import Welcome from '../views/Welcome.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const RouterMain = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Welcome',
+    component: Welcome
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+
 ]
 
-const router = new VueRouter({
+let allRoutes = []
+allRoutes = allRoutes.concat(RouterMain, RouterDashboard)
+const routes = allRoutes
+
+const routers = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
 })
 
-export default router
+export default routers
